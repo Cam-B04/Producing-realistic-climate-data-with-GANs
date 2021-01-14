b87afe20e6f904a9f75494208eba4c2260fd781c
Camille Besombes <besombes@cerfacs.fr>
2021-01-14 11:29:57 +0100

    cleaning

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Producing-realistic-climate-data-with-GANs
+
 Associated repository to the article Producing realistic climate data (10.5281/zenodo.4436274)
--- src/preparation/OPM_GAN.py
+++
@@ -1,7 +0,0 @@
-'''
-OPM_GAN is the main script for a program that is doing the inference neural network training using OPM to condition the generation on the results
-
-
-
-
-'''
